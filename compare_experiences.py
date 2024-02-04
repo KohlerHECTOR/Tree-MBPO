00dfdb36a5a48667f3fa981d720507e7655d297b
KohlerHECTOR <hector.kohler@inria.fr>
2024-02-04 15:23:12 +0100

    created exp comparator

---
+++ compare_experiences.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import sys
+import numpy as np
+import time
+import os
+
+args = sys.argv[1:]
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
+compare_folder = "Experience_Results/Comparison-"+timestr
+os.makedirs(compare_folder, exist_ok=True)
+
+
+### Evals
+for exp_name in args:
+    exp_name = "Experience_Results/" + exp_name
+    evals = np.loadtxt(exp_name + "/evals")
+    plt.plot(np.arange(len(evals)), evals, label=exp_name)
+plt.ylabel("Avg Cumulative Reward on Real Env")
+plt.xlabel("Iter")
+plt.grid()
+plt.legend()
+plt.savefig(compare_folder + "/evals.png")
+plt.clf()
+### Times
+for exp_name in args:
+    exp_name = "Experience_Results/" + exp_name
+    evals = np.loadtxt(exp_name + "/evals")
+    times = np.loadtxt(exp_name + "/times")
+    plt.plot(times, evals, label=exp_name)
+plt.ylabel("Avg Cumulative Reward on Real Env")
+plt.xlabel("Second")
+plt.grid()
+plt.legend()
+plt.savefig(compare_folder + "/times.png")
