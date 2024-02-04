310babb6a1211e718483efb413082502afffc905
KohlerHECTOR <hector.kohler@inria.fr>
2024-02-04 17:57:37 +0100

    clean

--- mbpo/mbpo_agent.py
+++ mbpo/mbpo_agent.py
@@ -33,6 +33,12 @@ class MBPOAgent:
     def add_new_transi(self, s: np.ndarray):
         action = self.agent.predict(th.FloatTensor(s.reshape(1, -1)), deterministic=False)[0][0]
         s_next, r, term, trunc, _ = self.env.step(action)
+
+        self.S = np.concatenate((self.S, s.reshape(1, -1)))
+        self.A = np.concatenate((self.A, action.reshape(1, -1)))
+        self.R = np.concatenate((self.R, np.array(r).reshape(-1, 1)))
+        self.Snext = np.concatenate((self.Snext, s_next.reshape(1, -1)))
+        self.Term = np.concatenate((self.Term, np.array(term, dtype=np.int8).reshape(-1, 1)))
         return action, r, s_next, term, trunc
         
     def learn(self, iter: int = 10):
@@ -40,7 +46,7 @@ class MBPOAgent:
         self.times = []
         self.init_real_data()
         start = time.time()
-        for i in trange(iter):
+        for i in range(iter):
             self.transi.fit(self.S, self.A, self.R, self.Snext)
             self.done.fit(self.S, self.A, self.R, self.Snext, self.Term)
             self.model_env = make_env(
@@ -51,8 +57,10 @@ class MBPOAgent:
                 agent_kwargs = dict(
                     policy="MlpPolicy",
                     env=self.model_env,
-                    train_freq=(1, "step"),
-                    gradient_steps=40
+                    train_freq=(400, "step"),
+                    gradient_steps=40,
+                    learning_starts=0,
+                    verbose=1
                 )
                 self.agent = self.agent(**agent_kwargs)
                 ### Init agent for first time ####
@@ -60,26 +68,20 @@ class MBPOAgent:
                 self.agent.env = self.model_env
             s, _ = self.env.reset()
             cum_r = 0
-            for step in range(1000):
-                a, r, snext, term, trunc = self.add_new_transi(s)
+            for j in trange(1000): # Real env steps
+                _, r, snext, term, trunc = self.add_new_transi(s)
                 cum_r += r
-                self.S = np.concatenate((self.S, s.reshape(1, -1)))
-                self.A = np.concatenate((self.A, a.reshape(1, -1)))
-                self.R = np.concatenate((self.R, np.array(r).reshape(-1, 1)))
-                self.Snext = np.concatenate((self.Snext, snext.reshape(1, -1)))
-                self.Term = np.concatenate((self.Term, np.array(term, dtype=np.int8).reshape(-1, 1)))
                 if term or trunc:
                     s, _ = self.env.reset()
                     self.evals.append(cum_r)
                     print("Perf Real Env {}".format(self.evals[-1]))
                     self.times.append(time.time() - start)
                     cum_r = 0
-                
 
                 self.model_env = make_env(self.env, self.S, self.transi, self.done, self.k)   
                 self.agent.env = self.model_env
 
-                self.agent.learn(total_timesteps=400)
+                self.agent.learn(total_timesteps=401)
 
                 s = snext
                 
